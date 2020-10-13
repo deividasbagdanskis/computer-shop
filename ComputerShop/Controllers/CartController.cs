b4c8356dc210493002131bdc53be59bc38440735
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-10-13 17:45:28 +0300

    Create cart controller

---
+++ ComputerShop/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ComputerShop.Context;
+using ComputerShop.Models;
+
+namespace ComputerShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ComputerShopContext _context;
+
+        public CartController(ComputerShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cart
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        // POST: api/Cart
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+    }
+}
