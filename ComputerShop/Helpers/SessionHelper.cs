d6a83993084c221ad903a6124deeb1f46bd2b86e
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-10-13 18:01:52 +0300

    Create session helper

---
+++ ComputerShop/Helpers/SessionHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace ComputerShop.Helpers
+{
+    public static class SessionHelper
+    {
+        public static void WriteToSession(ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T ReadFromSession<T>(ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
