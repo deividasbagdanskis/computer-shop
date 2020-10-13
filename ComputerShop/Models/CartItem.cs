a0d885ed195e7f266217511cc8ffc8951c643fb6
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-10-13 18:13:55 +0300

    Create cart item model

---
+++ ComputerShop/Models/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ComputerShop.Models
+{
+    [NotMapped]
+    public class CartItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
