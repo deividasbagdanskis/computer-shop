2a36cd6f8b411913643966226b62bd0332cbc4b0
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-11-09 19:55:48 +0200

    Create ComputerShopStockApi ef context

---
+++ ComputerShop.Stock.Api/Context/ComputerShopStockApiContext.cs
@@ -0,0 +1,15 @@
+﻿using ComputerShop.Stock.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerShop.Stock.Api.Context
+{
+    public class ComputerShopStockApiContext : DbContext
+    {
+        public ComputerShopStockApiContext(DbContextOptions<ComputerShopStockApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StockItem> StockItem { get; set; }
+    }
+}
--- ComputerShop.Stock.Api/Startup.cs
+++ ComputerShop.Stock.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ComputerShop.Stock.Api.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ComputerShop.Stock.Api
 {
@@ -26,6 +21,9 @@ namespace ComputerShop.Stock.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ComputerShopStockApiContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ComputerShopStockApiContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
