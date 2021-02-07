98b58df3eb9f411c95b91d0cc52f56388131dc3c
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2021-02-07 18:25:39 +0200

    Create data seeding migration for stock api

--- ComputerShop.Stock.Api/Context/ComputerShopStockApiContext.cs
+++ ComputerShop.Stock.Api/Context/ComputerShopStockApiContext.cs
@@ -10,6 +10,25 @@ namespace ComputerShop.Stock.Api.Context
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<StockItem>().HasData(new[]
+            {
+                new StockItem() { Id = 1, ProductId = 1, AmountInStock = 5 },
+                new StockItem() { Id = 2, ProductId = 2, AmountInStock = 9 },
+                new StockItem() { Id = 3, ProductId = 3, AmountInStock = 7 },
+                new StockItem() { Id = 4, ProductId = 4, AmountInStock = 14 },
+                new StockItem() { Id = 5, ProductId = 5, AmountInStock = 8 },
+                new StockItem() { Id = 6, ProductId = 6, AmountInStock = 0 },
+                new StockItem() { Id = 7, ProductId = 7, AmountInStock = 6 },
+                new StockItem() { Id = 8, ProductId = 8, AmountInStock = 12 },
+                new StockItem() { Id = 9, ProductId = 9, AmountInStock = 3 },
+                new StockItem() { Id = 10, ProductId = 10, AmountInStock = 4 }
+            });
+        }
+
         public DbSet<StockItem> StockItem { get; set; }
     }
 }
---
+++ ComputerShop.Stock.Api/Migrations/20210207155654_InsertInitialData.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ComputerShop.Stock.Api.Migrations
+{
+    public partial class InsertInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "StockItem",
+                columns: new[] { "Id", "AmountInStock", "ProductId" },
+                values: new object[,]
+                {
+                    { 1, 5, 1 },
+                    { 2, 9, 2 },
+                    { 3, 7, 3 },
+                    { 4, 14, 4 },
+                    { 5, 8, 5 },
+                    { 6, 0, 6 },
+                    { 7, 6, 7 },
+                    { 8, 12, 8 },
+                    { 9, 3, 9 },
+                    { 10, 4, 10 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "StockItem",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "StockItem",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "StockItem",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "StockItem",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "StockItem",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "StockItem",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "StockItem",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "StockItem",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "StockItem",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "StockItem",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
